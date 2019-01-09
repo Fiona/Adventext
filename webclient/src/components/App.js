2e8afaffa0df600ec21f777f7f876d753b70b2df
Fiona Summers <stompyblondie@gmail.com>
2019-01-09 23:13:03 +0000

    Added react-router and set up basic route

---
+++ webclient/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Router from './Router';
+
+function App()
+{
+    return (
+      <div className="App">
+          <header className="App-header">
+          </header>
+          <Router />
+      </div>
+    );
+}
+
+export default App;
