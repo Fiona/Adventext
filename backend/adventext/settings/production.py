e40009f02178d956e03cbbf9804577c76839884c
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-26 06:28:47 +0800

    Delete backend directory

--- backend/adventext/settings/production.py
+++
@@ -1,5 +0,0 @@
-from .common import *
-
-DEBUG = False
-SECRET_KEY = env('DJANGO_SECRET_KEY')
-SIMPLE_JWT['SIGNING_KEY'] = env('JWT_SIGNING_KEY')
--- backend/adventext/urls.py
+++
@@ -1,9 +0,0 @@
-"""Adventext URL Configuration"""
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    path('api/auth/', include('adventext.auth.urls'))
-]
